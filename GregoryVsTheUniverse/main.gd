a74e426ba0395b76529bfc0d22ce9eae4fbdc24a
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2026-02-21 17:43:06 -0600

    Put In Music

--- GregoryVsTheUniverse/main.gd
+++ GregoryVsTheUniverse/main.gd
@@ -9,11 +9,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Globals.stage == -1:
+		AudioServer.get_bus_effect(1,0).cutoff_hz = 1000
 		$MainCam.position = Vector2(640,360)
 		if Globals.playerPosition.y < -50:
 			$Player.position = Vector2(450,-1200)
 			Globals.stage = 0
 	else:
+		if Globals.stage == 0:
+			AudioServer.get_bus_effect(1,0).cutoff_hz = 2500
+		else:
+			AudioServer.get_bus_effect(1,0).cutoff_hz = 20000
 		var camOffset = get_viewport().get_mouse_position() - Vector2(640,360)
 		$MainCam.position = Globals.playerPosition + camOffset/4.0 + Vector2.from_angle(randf_range(0,10))*Globals.shakeEffect*30.0
 		$MainCam.zoom = Vector2.ONE + Vector2(0.1,0.1)*Globals.shakeEffect
@@ -24,6 +29,7 @@ func _on_first_elevator_body_entered(body):
 		Globals.enemyGoal = $Stages/Stage1.enemyCount
 		Globals.killCount = 0
 		$Player.position = $Stages/Stage1.position + Vector2(150,0)
+		Globals.stage = 1
 		$Stages/Stage2.generate()
 
 
@@ -31,4 +37,5 @@ func _on_stage_1_next_stage():
 	Globals.enemyGoal = $Stages/Stage2.enemyCount
 	Globals.killCount = 0
 	$Player.position = $Stages/Stage2.position + Vector2(150,0)
+	Globals.stage = 2
 	$Stages/Stage3.generate()

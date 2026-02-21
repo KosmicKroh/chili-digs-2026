2a6dee21c3bedd7f8a1ec4fac092a1cdf9329903
pppp111164730 <122899052+pppp111164730@users.noreply.github.com>
2026-02-21 09:00:02 -0600

    removed yellow warnings

--- GregoryVsTheUniverse/R_Safe_Folder/bullet.gd
+++ GregoryVsTheUniverse/R_Safe_Folder/bullet.gd
@@ -5,12 +5,12 @@ class_name Bullet extends Sprite2D
 const BULLET_SCENE: PackedScene = preload("res://R_Safe_Folder/Bullet.tscn")
 
 static func newBullet(Position: Vector2, Rotation: float = 0, LifeTime: float = 10, Speed: int = 100) -> Sprite2D:
-	var newBullet = BULLET_SCENE.instantiate()
-	newBullet.position = Position
-	newBullet.rotation = Rotation
-	newBullet.lifeTime = LifeTime
-	newBullet.speed = Speed
-	return newBullet
+	var newbullet = BULLET_SCENE.instantiate()
+	newbullet.position = Position
+	newbullet.rotation = Rotation
+	newbullet.lifeTime = LifeTime
+	newbullet.speed = Speed
+	return newbullet
 
 func _process(delta: float) -> void:
 	lifeTime -= delta
--- GregoryVsTheUniverse/R_Safe_Folder/hit_box.gd
+++ GregoryVsTheUniverse/R_Safe_Folder/hit_box.gd
@@ -1,8 +1,8 @@
 extends Area2D
 @export var damage: int
 
-func _on_area_entered(area: Area2D) -> void:
+func _on_area_entered(_area: Area2D) -> void:
 	get_parent().queue_free()
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	get_parent().queue_free()

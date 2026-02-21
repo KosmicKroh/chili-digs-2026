a8f7c8a9c20d05ca2d8177300696941bc39f0fab
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2026-02-21 17:10:48 -0600

    Did some cool camera stuff

--- GregoryVsTheUniverse/main.gd
+++ GregoryVsTheUniverse/main.gd
@@ -14,9 +14,21 @@ func _process(delta):
 			$Player.position = Vector2(450,-1200)
 			Globals.stage = 0
 	else:
-		$MainCam.position = Globals.playerPosition
+		var camOffset = get_viewport().get_mouse_position() - Vector2(640,360)
+		$MainCam.position = Globals.playerPosition + camOffset/4.0 + Vector2.from_angle(randf_range(0,10))*Globals.shakeEffect*30.0
+		$MainCam.zoom = Vector2.ONE + Vector2(0.1,0.1)*Globals.shakeEffect
 
 
 func _on_first_elevator_body_entered(body):
 	if "player" in body:
-		$Player.position = Vector2(150,-10000)
+		Globals.enemyGoal = $Stages/Stage1.enemyCount
+		Globals.killCount = 0
+		$Player.position = $Stages/Stage1.position + Vector2(150,0)
+		$Stages/Stage2.generate()
+
+
+func _on_stage_1_next_stage():
+	Globals.enemyGoal = $Stages/Stage2.enemyCount
+	Globals.killCount = 0
+	$Player.position = $Stages/Stage2.position + Vector2(150,0)
+	$Stages/Stage3.generate()

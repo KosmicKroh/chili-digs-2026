4b76ef9df24262bedc9d0fbf9ad337c890072c73
SanjayVikash <sanjayvikashvasidhar@gmail.com>
2026-02-21 11:56:43 -0600

    Added Stamina bar and enemy counter bar

--- GregoryVsTheUniverse/J_Safe_Folder/hud.gd
+++ GregoryVsTheUniverse/J_Safe_Folder/hud.gd
@@ -9,3 +9,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$HealthBar.value = clamp(Globals.playerHealth,0,100.0)
+	$StaminaBar.value = clamp(Globals.playerStamina,0,100.0)
+	$EnemyCounterBar.max_value = Globals.enemyGoal
+	$EnemyCounterBar.value = clamp(Globals.killCount,0,Globals.enemyGoal)

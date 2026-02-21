49a90c60a9b1d893b46b40a2a13c6093da4e7c2b
pppp111164730 <122899052+pppp111164730@users.noreply.github.com>
2026-02-21 09:15:03 -0600

    player

--- GregoryVsTheUniverse/R_Safe_Folder/bullet.gd
+++ GregoryVsTheUniverse/R_Safe_Folder/bullet.gd
@@ -2,15 +2,20 @@ class_name Bullet extends Sprite2D
 
 @export var lifeTime: float
 @export var speed: int
+@export var Damage: Area2D
 const BULLET_SCENE: PackedScene = preload("res://R_Safe_Folder/Bullet.tscn")
 
-static func newBullet(Position: Vector2, Rotation: float = 0, LifeTime: float = 10, Speed: int = 100) -> Sprite2D:
+static func newBullet(Position: Vector2, Rotation: float = 0, Bullet_Damage: int = 1, LifeTime: float = 10, Speed: int = 100) -> Sprite2D:
 	var newbullet = BULLET_SCENE.instantiate()
 	newbullet.position = Position
 	newbullet.rotation = Rotation
 	newbullet.lifeTime = LifeTime
 	newbullet.speed = Speed
+	newbullet.Damage = Bullet_Damage
 	return newbullet
+
+func _ready() -> void:
+	get_node("HitBox").damage = Damage
 
 func _process(delta: float) -> void:
 	lifeTime -= delta

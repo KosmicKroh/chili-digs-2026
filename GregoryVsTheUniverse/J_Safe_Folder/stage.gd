877fb3ca94e287aefe3af40771695d7074adcdfd
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2026-02-21 15:58:36 -0600

    Worked on Main

--- GregoryVsTheUniverse/J_Safe_Folder/stage.gd
+++ GregoryVsTheUniverse/J_Safe_Folder/stage.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+signal next_stage
 var elevator = load("res://J_Safe_Folder/room_elevator.tscn")
 var room_layouts = [
 	load("res://J_Safe_Folder/room_bullpin.tscn"),
@@ -7,6 +8,10 @@ var room_layouts = [
 	load("res://J_Safe_Folder/room_hallway.tscn"),
 	load("res://J_Safe_Folder/room_waiting.tscn"),
 	load("res://J_Safe_Folder/room_office.tscn")
+]
+var enemies = [
+	preload("res://R_Safe_Folder/Enemy_Melee.tscn"),
+	preload("res://R_Safe_Folder/Enemy_Ranged.tscn")
 ]
 
 @export var roomCount:int = 30
@@ -25,10 +30,16 @@ func _ready():
 	for exit in spawnRoom.find_child("Exits").get_children():
 		availableExits.append(exit)
 	roomCount -= 1
-	generate()
 
 func generate():
 	readyToGenerate = true
+
+func spawn_enemies():
+	enemySpawns.shuffle()
+	for e in min(len(enemySpawns),enemyCount):
+		var newEnemy = enemies.pick_random().instantiate()
+		add_child(newEnemy)
+		newEnemy.global_transform = enemySpawns[e].global_transform
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
@@ -44,6 +55,8 @@ func _physics_process(_delta):
 				for spawn in latestRoom.find_child("Spawns").get_children():
 					enemySpawns.append(spawn)
 				roomCount -= 1
+				if roomCount == 0:
+					spawn_enemies()
 		if roomCount > 0:
 			firstPass = true
 			var newRoom = room_layouts.pick_random().instantiate()
@@ -58,3 +71,8 @@ func _physics_process(_delta):
 			latestRoom = newRoom
 			
 		
+
+
+func _on_elevator_area_body_entered(body):
+	if "player" in body and Globals.killCount >= Globals.enemyGoal:
+		next_stage.emit()
---
+++ GregoryVsTheUniverse/main.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Stages/Stage1.generate()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Globals.stage == -1:
+		$MainCam.position = Vector2(640,360)
+		if $Player.position.y < -50:
+			$Player.position = Vector2(450,-1200)
+			Globals.stage = 0
+	else:
+		$MainCam.position = $Player.position
+
+
+func _on_first_elevator_body_entered(body):
+	if "player" in body:
+		$Player.position = Vector2(150,-10000)
